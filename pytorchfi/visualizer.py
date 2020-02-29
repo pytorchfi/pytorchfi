378af24779befd5bde366e4b8ab1d80ef4d171b3
Harrison McCarty <mccarth@purdue.edu>
2020-02-29 00:29:40 -0600

    Adding CSV importing

--- pytorchfi/visualizer.py
+++ pytorchfi/visualizer.py
@@ -1,1 +1,11 @@
 import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+layer_data = np.genfromtxt('test.csv', delimiter=',')
+fig, ax = plt.subplots()
+im = ax.imshow(layer_data)
+
+plt.show()
+
+print(layer_data)
