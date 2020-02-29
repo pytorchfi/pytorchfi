bcb83cb2c7bce40eebce9c7398ddbd701b110092
Andrew Masek <amasek@purdue.edu>
2020-02-29 17:17:30 -0500

    Added slider and prepared for bar graph function

--- pytorchfi/visualizer.py
+++ pytorchfi/visualizer.py
@@ -1,6 +1,7 @@
 import matplotlib
 import numpy as np
 import matplotlib.image as mpimg 
+from matplotlib.widgets import Slider
 import matplotlib.pyplot as plt
 import os
 
@@ -8,21 +9,37 @@ images = os.listdir('Images/')
 
 #data = np.genfromtxt('test.csv', delimiter=',')
 
+chart_count = len(images)
+a_min = 1
+a_max = chart_count
+a_init = 0
+
+chart_list = list(range(0, chart_count))
+
 fig = plt.figure()
 
-row_count = len(images)
+#print("Number of images is " + str(len(images)) + "\n")
 
-for row in range(1, row_count):
+for chart in range(0, chart_count):
 
-	ax = fig.add_subplot(row_count, 1, row)
+	chart_list[chart] = fig.add_subplot(chart_count, 1, chart)
+	chart_list[chart].axis('off')
 
-	ax.imshow(mpimg.imread('./Images/' + images[row - 1]), interpolation = 'bilinear')
+slider_ax = plt.axes([0.1, 0.12, 0.8, 0.05])
 
-	ax.axis('off')
+
+a_slider = Slider(slider_ax, 'Layer', a_min, a_max, valinit=a_init, valfmt='%d')
+
+def update(layer):
+	print("UPDATE RECEIVED: CURRENT LAYER IS ::" + str(layer) + " \n")
+	chart_list[int(layer - 1)].imshow(mpimg.imread('./Images/' + images[int(layer) - 1]), interpolation = 'bilinear')
+	chart_list[int(layer) - 1].axis('off')
+
+
+a_slider.on_changed(update)
+
+
 	
-
-fig.subplots_adjust(hspace=0)
-
 fig.suptitle('Vulnerability Visualization')
 
 plt.show()
