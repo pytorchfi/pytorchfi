d54f31fc76ab4449a3a4917a0e3e77795f8a7f03
Harrison McCarty <mccarth@purdue.edu>
2020-02-29 12:22:53 -0600

    Saving images of heatmap

--- pytorchfi/visualizer.py
+++ pytorchfi/visualizer.py
@@ -2,19 +2,30 @@ import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.genfromtxt('test.csv', delimiter=',')
+def save_image(numpy_data, layer_count, last):
+    fig = plt.figure(figsize=(20, 20))
+    ax = fig.add_subplot(111)
+    ax.axis('off')
+    im = ax.imshow(np.array(graph_data), interpolation='bilinear')
+    if last:
+        cbar = ax.figure.colorbar(im, ax=ax, cmap='YlGn')
+    fig.set_size_inches(18.5, 10.5, forward=True)
+    plt.savefig("map" + str(layer_count))
 
-intensity_data = [[]]
-curr_layer = 0
-for i in range(len(data)):
-    if data[i][1] != curr_layer:
-        curr_layer += 1
-        intensity_data.append([])
-    intensity_data[curr_layer].append(data[i][3])
-print(intensity_data)
-graph = np.array([np.array(x) for x in intensity_data])
-fig, ax = plt.subplots()
-im = ax.imshow(intensity_data, interpolation='bilinear')
-cbar = ax.figure.colorbar(im, ax=ax, cmap='YlGn')
-cbar.ax.set_ylabel('Vulnerability', rotation=-90, va='bottom')
-plt.savefig("map")
+try:
+    input_file = open("alexnet.csv")
+    name_line = input_file.readline()
+    csv_line = input_file.readline()
+    layer_count = 0
+    graph_data = [[]]
+    while csv_line != "":
+        csv_arr = csv_line.split(",")
+        if int(csv_arr[1]) != layer_count:
+            save_image(np.array(graph_data), layer_count, False)
+            graph_data = [[]]
+            layer_count += 1
+        graph_data[0].append(float(csv_arr[3]))
+        csv_line = input_file.readline()
+    save_image(np.array(graph_data), layer_count, True)
+finally:
+    input_file.close()
