6d3f3bbb1b80ed530150c157943eb5d3efd6a599
Harrison McCarty <mccarth@purdue.edu>
2020-03-01 02:56:20 -0600

    Format using Black

--- pytorchfi/visualizer.py
+++ pytorchfi/visualizer.py
@@ -4,80 +4,95 @@ import matplotlib.pyplot as plt
 import os
 import sys
 
-'''
+"""
 Creates heatmap of the vulnerabilities for a single layer
-'''
+"""
+
+
 def heat_map(numpy_data, layer_count, output_dir):
     fig = plt.figure(figsize=(20, 20))
     ax = fig.add_subplot(111)
-    ax.axis('off')
-    im = ax.imshow(numpy_data, interpolation='bilinear')
+    ax.axis("off")
+    im = ax.imshow(numpy_data, interpolation="bilinear")
     try:
-        plt.savefig(output_dir + '/Layer-' + str(layer_count) + '-Heat Map')
+        plt.savefig(output_dir + "/Layer-" + str(layer_count) + "-Heat Map")
     except FileNotFoundError:
-        os.mkdir('Graphs')
-        plt.savefig(output_dir + '/Layer-' + str(layer_count) + '-Heat Map')
+        os.mkdir("Graphs")
+        plt.savefig(output_dir + "/Layer-" + str(layer_count) + "-Heat Map")
 
-'''
+
+"""
 Creates bar graph sorted by layer number
-'''
+"""
+
+
 def sequential_bar_graph(numpy_data, layer_count, output_dir):
     fig = plt.figure(figsize=(20, 20))
     ax = fig.add_subplot(111)
     data = list(numpy_data)[0]
     cols = list(range(1, len(data) + 1))
-    ax.set_ylabel('Level of Vulnerability')
-    ax.set_title('Layer ' + str(layer_count + 1) + ' Vulnerability per Feature Frame')
+    ax.set_ylabel("Level of Vulnerability")
+    ax.set_title("Layer " + str(layer_count + 1) + " Vulnerability per Feature Frame")
     ax.bar(cols, data)
     try:
-        plt.savefig(output_dir + '/Layer-' + str(layer_count + 1) + '-Seq-Bar-Graph')
+        plt.savefig(output_dir + "/Layer-" + str(layer_count + 1) + "-Seq-Bar-Graph")
     except FileNotFoundError:
-        os.mkdir('Graphs')
-        plt.savefig(output_dir + '/Layer-' + str(layer_count + 1) + '-Seq-Bar-Graph')
+        os.mkdir("Graphs")
+        plt.savefig(output_dir + "/Layer-" + str(layer_count + 1) + "-Seq-Bar-Graph")
 
-'''
+
+"""
 Creates bar graph sorted by vulnerability
-'''
+"""
+
+
 def nonsequential_bar_graph(numpy_data, layer_count, output_dir):
     fig = plt.figure(figsize=(20, 20))
     ax = fig.add_subplot(111)
     data = list(numpy_data)[0]
     cols = list(range(1, len(data) + 1))
     quicksort(data, 0, len(data) - 1)
-    ax.set_ylabel('Level of Vulnerability')
-    ax.set_title('Layer ' + str(layer_count) + ' Vulnerability per Feature Frame')
+    ax.set_ylabel("Level of Vulnerability")
+    ax.set_title("Layer " + str(layer_count) + " Vulnerability per Feature Frame")
     ax.bar(cols, data)
-    plt.savefig(output_dir + '/Layer-' + str(layer_count) + '-NonSeq-Bar-Graph')
+    plt.savefig(output_dir + "/Layer-" + str(layer_count) + "-NonSeq-Bar-Graph")
     try:
-        plt.savefig(output_dir + '/Layer-' + str(layer_count) + '-NonSeq-Bar-Graph')
+        plt.savefig(output_dir + "/Layer-" + str(layer_count) + "-NonSeq-Bar-Graph")
     except FileNotFoundError:
-        os.mkdir('Graphs')
-        plt.savefig('Graphs/Layer-' + str(layer_count) + '-NonSeq-Bar-Graph')
+        os.mkdir("Graphs")
+        plt.savefig("Graphs/Layer-" + str(layer_count) + "-NonSeq-Bar-Graph")
 
-'''
+
+"""
 Quick sort functions
-'''
+"""
+
+
 def partition(arr, low, high):
-    i = (low-1)
+    i = low - 1
     pivot = arr[high]
 
     for j in range(low, high):
         if arr[j] <= pivot:
-            i = i+1
+            i = i + 1
             arr[i], arr[j] = arr[j], arr[i]
-    arr[i+1], arr[high] = arr[high], arr[i+1]
-    return ( i+1)
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
 
 def quicksort(arr, low, high):
     if low < high:
         pi = partition(arr, low, high)
-        quicksort(arr, low, pi-1)
-        quicksort(arr, pi+1, high)
+        quicksort(arr, low, pi - 1)
+        quicksort(arr, pi + 1, high)
 
-'''
+
+"""
 Gets the data points of each layer, then calls the generator
 to output the graphs of each layer
-'''
+"""
+
+
 def generate_graph(file_name, generator, output_dir):
     try:
         input_file = open(file_name)
@@ -99,9 +114,10 @@ def generate_graph(file_name, generator, output_dir):
     finally:
         input_file.close()
 
-'''
+
+"""
 Parameters
-'''
+"""
 # Graph types: seq-bar, non-seq-bar, heat-map
 graph_type_param = "type="
 file_name_param = "file="
@@ -113,20 +129,19 @@ if __name__ == "__main__":
     output_dir = ""
     for arg in sys.argv[1:]:
         if graph_type_param in arg:
-            graph_type = arg[len(graph_type_param):]
+            graph_type = arg[len(graph_type_param) :]
         elif file_name_param in arg:
-            file_name = arg[len(file_name_param):]
+            file_name = arg[len(file_name_param) :]
         elif output_dir in arg:
-            output_dir = arg[len(output_dir_param):]
+            output_dir = arg[len(output_dir_param) :]
         else:
             print("Invalid parameter")
 
     if graph_type == "seq-bar":
-          generate_graph(file_name, sequential_bar_graph, output_dir)
+        generate_graph(file_name, sequential_bar_graph, output_dir)
     elif graph_type == "non-seq-bar":
-          generate_graph(file_name, nonsequential_bar_graph, output_dir)
+        generate_graph(file_name, nonsequential_bar_graph, output_dir)
     elif graph_type == "heat-map":
-          generate_graph(file_name, heat_map, output_dir)
+        generate_graph(file_name, heat_map, output_dir)
     else:
-          print("Invalid graph type")
-
+        print("Invalid graph type")
