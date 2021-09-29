93739f4e7b43bedbd4f5929a13bc61940dac01cc
Abdulrahman Mahmoud <lazio2013@gmail.com>
2021-09-29 13:07:27 -0500

    Actually fix issue with all (with unit test). Naming fixes

--- test/unit_tests/test_layers.py
+++ test/unit_tests/test_layers.py
@@ -86,6 +86,37 @@ class TestLayers:
         if p.get_layer_type(20) != torch.nn.Linear:
             raise AssertionError
 
+    def test_inj_all_layers_injections(self):
+        p = fault_injection(
+            self.model,
+            self.BATCH_SIZE,
+            layer_types=["all"],
+            use_cuda=self.USE_GPU,
+        )
+
+        (b, layer, C, H, W, err_val) = (
+            [0,1,2,3], [0,1,2,20], [4,4,0,4], [2, 2, 2, None], [4, 2, 2, None],
+            [10000, 10000, 10000, 1000])
+
+        inj = p.declare_neuron_fi(
+            batch=b, layer_num=layer, dim1=C, dim2=H, dim3=W, value=err_val
+        )
+        inj_output = inj(self.IMAGE)
+        inj_softmax = self.softmax(inj_output)
+        inj_label_0 = list(torch.argmax(inj_softmax, dim=1))[0].item()
+        inj_label_1 = list(torch.argmax(inj_softmax, dim=1))[1].item()
+        inj_label_2 = list(torch.argmax(inj_softmax, dim=1))[2].item()
+        inj_label_3 = list(torch.argmax(inj_softmax, dim=1))[3].item()
+
+        if inj_label_0 != 722:
+            raise AssertionError
+        if inj_label_1 != 723:
+            raise AssertionError
+        if inj_label_2 != 968:
+            raise AssertionError
+        if inj_label_3 != 4:
+            raise AssertionError
+
     def test_single_linear_neuron_inj(self):
         p = fault_injection(
             self.model,
